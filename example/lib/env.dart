980cbb291f284699e72036b31e6b40af86ddf84f
Yannick Stolle <yannick.stolle@merckgroup.com>
2022-05-02 15:07:59 +0200

    chore: Adjusted gitignore file

--- example/lib/env.dart
+++
@@ -1,6 +0,0 @@
-// This file contains the configuration for the app.
-
-String discoveryUrl =
-    "https://login.emddigital.com/.well-known/openid-configuration";
-String clientId = "pkt-ALEUvLknGqosN8w7cA";
-String redirectUrl = "com.your.awesome-app://login-callback";
