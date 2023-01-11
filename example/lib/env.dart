43ed5322587d110cc99bccf1455e8cc7ebbabc9a
Yannick Stolle <yannick.stolle@merckgroup.com>
2023-01-11 16:42:20 +0100

    fix: provide default env file for example

---
+++ example/lib/env.dart
@@ -0,0 +1,6 @@
+// This file contains the configuration for the app.
+
+String discoveryUrl = "https://login.emddigital.com/.well-known/openid-configuration";
+String clientId = "pkt-ALEUvLknGqosN8w7cA";
+String redirectUrl = "com.your.awesome-app://login-callback";
+List<String> scopes = ["openid", "email"];
